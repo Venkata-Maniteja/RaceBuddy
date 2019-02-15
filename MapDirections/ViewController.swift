988d5b7965d9877a82357f1bb762a7a7a20a6f30
Rupika Sompalli <rupikasompalli@rupikas-mac-mini.local>
2019-02-15 09:01:32 -0500

    Initial Commit

---
+++ MapDirections/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapDirections
+//
+//  Created by Rupika Sompalli on 15/02/19.
+//  Copyright © 2019 Venkata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
